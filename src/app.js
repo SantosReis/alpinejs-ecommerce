b84308bb7c47c5562adbd147190e62853bc9f307
Marcus Reis <mvsantosreis@gmail.com>
2024-12-22 10:30:27 +0000

    Designing into stores and components

--- src/app.js
+++ src/app.js
@@ -1,68 +1,77 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('index', () => ({
+  Alpine.store('header', {
     cartItems: 0,
     watchingItems: [],
-    quantity: 1,
     get watchlistItems() {
       return this.watchingItems.length
     },
-    addToCart(quantity = 1) {
-      this.cartItems += parseInt(quantity)
-      this.toast.show('The item was added into the cart')
+  })
+
+  Alpine.store('toast', {
+    visible: false,
+    delay: 5000,
+    percent: 0,
+    interval: 0,
+    timeout: null,
+    message: null,
+    close() {
+      this.visible = false
+      clearInterval(this.interval)
+    },
+    show(message) {
+      this.visible = true
+      this.message = message
+
+      if (this.interval) {
+        clearInterval(this.interval)
+        this.timeout = null
+      }
+
+      if (this.timeout) {
+        clearTimeout(this.timeout)
+        this.interval = null
+      }
+
+      this.timeout = setTimeout(() => {
+        this.visible = false
+        this.timeout = null
+      }, this.delay)
+      const startDate = Date.now()
+      const futureDate = Date.now() + this.delay
+      this.interval = setInterval(() => {
+        const date = Date.now()
+        this.percent = ((date - startDate) * 100) / (futureDate - startDate)
+        if (this.percent >= 100) {
+          clearInterval(this.interval)
+          this.interval = null
+        }
+      }, 30)
+    },
+  })
+
+  Alpine.data('productItem', () => ({
+    quantity: 1,
+    get watchlistItems() {
+      return this.$store.watchlistItems
     },
     addToWatchlist(id) {
       // if (this.watchingItems.includes(id)) return
-      if (this.watchingItems.includes(id)) {
-        this.watchingItems.splice(this.watchingItems.indexOf(id))
-        this.toast.show('The item was remoted from your watchlist')
+      if (this.$store.header.watchingItems.includes(id)) {
+        this.$store.header.watchingItems.splice(
+          this.$store.header.watchingItems.indexOf(id)
+        )
+        this.$store.toast.show('The item was remoted from your watchlist')
       } else {
-        this.watchingItems.push(id)
-        this.toast.show('The item was added into your watchlist')
+        this.$store.header.watchingItems.push(id)
+        this.$store.toast.show('The item was added into your watchlist')
       }
     },
     isInWatchlist(id) {
-      return this.watchingItems.includes(id)
+      return this.$store.header.watchingItems.includes(id)
     },
-    toast: {
-      visible: false,
-      delay: 5000,
-      percent: 0,
-      interval: 0,
-      timeout: null,
-      message: null,
-      close() {
-        this.visible = false
-        clearInterval(this.interval)
-      },
-      show(message) {
-        this.visible = true
-        this.message = message
-
-        if (this.interval) {
-          clearInterval(this.interval)
-          this.timeout = null
-        }
-
-        if (this.timeout) {
-          clearTimeout(this.timeout)
-          this.interval = null
-        }
-
-        this.timeout = setTimeout(() => {
-          this.visible = false
-          this.timeout = null
-        }, this.delay)
-        const startDate = Date.now()
-        const futureDate = Date.now() + this.delay
-        this.interval = setInterval(() => {
-          const date = Date.now()
-          this.percent = ((date - startDate) * 100) / (futureDate - startDate)
-          if (this.percent >= 100) {
-            clearInterval(this.interval)
-            this.interval = null
-          }
-        }, 30)
-      },
+    addToCart(quantity = 1) {
+      this.$store.header.cartItems += parseInt(quantity)
+      this.$store.toast.show('The item was added into the cart')
     },
   }))
 })
