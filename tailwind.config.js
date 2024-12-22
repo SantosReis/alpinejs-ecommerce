28d4370659b85da37b951d705f74753ccabad4cf
Marcus Reis <mvsantosreis@gmail.com>
2024-12-22 12:46:02 +0000

    feat Carousel with Tailwind aspect ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
   ],
-};
+}
