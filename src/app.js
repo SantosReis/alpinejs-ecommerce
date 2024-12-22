9c670eebddb23999dabef366af2846ffe947e6da
Marcus Reis <mvsantosreis@gmail.com>
2024-12-22 11:43:15 +0000

    Turning toast to component from store and applying dispatch

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ document.addEventListener('alpine:init', () => {
     },
   })
 
-  Alpine.store('toast', {
+  Alpine.data('toast', () => ({
     visible: false,
     delay: 5000,
     percent: 0,
@@ -47,7 +47,7 @@ document.addEventListener('alpine:init', () => {
         }
       }, 30)
     },
-  })
+  }))
 
   Alpine.data('productItem', () => ({
     quantity: 1,
@@ -55,16 +55,19 @@ document.addEventListener('alpine:init', () => {
       return this.$store.watchlistItems
     },
     addToWatchlist(id) {
-      // if (this.$store.header.watchingItems.includes(id)) return
       if (this.$store.header.watchingItems.includes(id)) {
         this.$store.header.watchingItems.splice(
           this.$store.header.watchingItems.indexOf(id),
           1
         )
-        this.$store.toast.show('The item was removed from your watchlist')
+        this.$dispatch('notify', {
+          message: 'The item was removed from your watchlist',
+        })
       } else {
         this.$store.header.watchingItems.push(id)
-        this.$store.toast.show('The item was added into your watchlist')
+        this.$dispatch('notify', {
+          message: 'The item was added into your watchlist',
+        })
       }
     },
     isInWatchlist(id) {
